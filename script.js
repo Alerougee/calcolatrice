ec17655d8352b3198ae7486613a25dafd4c7ea3b
Alerougee <alericcio08@gmail.com>
2026-01-08 12:31:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,87 @@
-window.onload = function () {
-    var schermo = document.getElementById("schermo");
+ var schermo = document.getElementById("schermo");
 
-    let Tempeval=0;
-
-    function aggiungi(s){
-        schermo.value += s;
-    }
-
-    function cancella(){
-        schermo.value = "";
-    }
-
-    function calcola(){
-        try{
-            schermo.value = eval(schermo.value);
-        } catch(e){
-            schermo.value = "errore";
+        function aggiungi(s) {
+            schermo.value += s;
         }
-    }
 
-    function radice(){
-            if(eval(schermo.value) < 0){
-                schermo.value = "errore"
-            }else{
-                schermo.value = Math.sqrt(eval(schermo.value));
+        function cancella() {
+            schermo.value = "";
+        }
+        function calcola() {
+        var testo = schermo.value;
+        var elementi = [];
+        var numero = "";
+        if (testo.startsWith("sin")) {
+          var n = parseFloat(testo.substring(3));
+          schermo.value = Math.sin(n * Math.PI / 180);
+          return;
+        }
+        if (testo.startsWith("cos")) {
+          var n = parseFloat(testo.substring(3));
+          schermo.value = Math.cos(n * Math.PI / 180);
+          return;
+        }
+        if (testo.startsWith("tan")) {
+          var n = parseFloat(testo.substring(3));
+          schermo.value = Math.tan(n * Math.PI / 180);
+          return;
+        }
+        if (testo.startsWith("√")) {
+          var n = parseFloat(testo.substring(1));
+          schermo.value = Math.sqrt(n);
+          return;
+        }
+        if (schermo.value.endsWith("%")) {
+          var numero = schermo.value.substring(0, schermo.value.length - 1);
+          numero = parseFloat(numero);
+          schermo.value = numero / 100;
+          return;
+        }
+        for (var i = 0; i < testo.length; i++) {
+          var c = testo[i];
+            if ((c >= '0' && c <= '9') || c == '.') {
+              numero += c;
+            } else {
+              elementi.push(numero);
+              elementi.push(c);
+              numero = "";
             }
-    }
-
-    function sin(){
-        schermo.value = Math.sin(eval(schermo.value));
-    }
-
-    function cos(){
-        schermo.value = Math.cos(eval(schermo.value));
-    }
-
-    function tan(){
-        schermo.value = Math.cos(eval(schermo.value));
-    }
-
-    function memoria(){
-        const val = parseFloat(schermo.value);
-        if(!isNaN(val)) {
-            Tempeval = val;
-        }else{
-            Tempeval=0;
         }
-        
+        elementi.push(numero);
+        for (var i = 1; i < elementi.length; i += 2) {
+          if (elementi[i] == '^') {
+            var a = parseFloat(elementi[i - 1]);
+            var b = parseFloat(elementi[i + 1]);
+            var risultato = Math.pow(a, b);
+            elementi.splice(i - 1, 3, risultato.toString());
+            i -= 2;
+          }
+        }
+        for (var i = 1; i < elementi.length; i += 2) {
+          if (elementi[i] == '*' || elementi[i] == '/') {
+            var a = parseFloat(elementi[i - 1]);
+            var b = parseFloat(elementi[i + 1]);
+            var risultato = 0;
+            if (elementi[i] == '*') {
+                risultato = a * b;
+            } else {
+                risultato = a / b;
+            }
+            elementi.splice(i - 1, 3, risultato.toString());
+            i -= 2;
+          }
+        }
+        for (var i = 1; i < elementi.length; i += 2) {
+          var a = parseFloat(elementi[i - 1]);
+          var b = parseFloat(elementi[i + 1]);
+          var risultato = 0;
+          if (elementi[i] == '+') {
+            risultato = a + b;
+          } else if (elementi[i] == '-') {
+            risultato = a - b;
+          }
+          elementi.splice(i - 1, 3, risultato.toString());
+          i -= 2;
+        }
+        schermo.value = elementi[0];
     }
-
-    function memoriastampa(){
-        schermo.value = Tempeval;
-    }
-
-    window.aggiungi = aggiungi;
-    window.cancella = cancella;
-    window.calcola = calcola;
-    window.radice = radice;
-    window.sin = sin;   
-    window.cos = cos;    
-    window.tan = tan;
-    window.memoria = memoria;
-    window.memoriastampa = memoriastampa;
-}
-
-
